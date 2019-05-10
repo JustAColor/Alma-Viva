8f00cd425f11912b25b255776d3be0a38afdf94a
juanSmira <juanmira720@gmail.com>
2019-05-10 14:29:48 -0500

    base de datos

---
+++ almaviva.sql
@@ -0,0 +1,118 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-05-2019 a las 21:29:29
+-- Versión del servidor: 10.1.39-MariaDB
+-- Versión de PHP: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `almaviva`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `infouser`
+--
+
+CREATE TABLE `infouser` (
+  `iduser` int(11) NOT NULL,
+  `Nombre` varchar(50) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `Apellido` varchar(50) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `telefono` int(11) NOT NULL,
+  `edad` int(11) NOT NULL,
+  `genero` varchar(1) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `idpais` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pais`
+--
+
+CREATE TABLE `pais` (
+  `idpais` int(11) NOT NULL,
+  `Pais` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `iduser` int(11) NOT NULL,
+  `username` varchar(20) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `password` varchar(20) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `tipoUser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `infouser`
+--
+ALTER TABLE `infouser`
+  ADD KEY `iduser` (`iduser`),
+  ADD KEY `idpais` (`idpais`);
+
+--
+-- Indices de la tabla `pais`
+--
+ALTER TABLE `pais`
+  ADD PRIMARY KEY (`idpais`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`iduser`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `pais`
+--
+ALTER TABLE `pais`
+  MODIFY `idpais` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `iduser` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `infouser`
+--
+ALTER TABLE `infouser`
+  ADD CONSTRAINT `infouser_ibfk_1` FOREIGN KEY (`iduser`) REFERENCES `usuario` (`iduser`),
+  ADD CONSTRAINT `infouser_ibfk_2` FOREIGN KEY (`idpais`) REFERENCES `pais` (`idpais`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
