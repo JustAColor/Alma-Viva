a2cd6d826ade54e455d9a19dc30bc182bd19c4b1
juanSmira <juanmira720@gmail.com>
2019-05-10 16:01:23 -0500

    login

---
+++ vistas/login.php
@@ -0,0 +1,43 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="estiloLogin.css">
+</head>
+<body>
+
+    <div class="modal-dialog text-center">
+      <div class="col-sm-8 main-section">
+        <div class="modal-content">
+          <form action="" class="col-12">
+            <div class="form-group">
+                <input type="text" name="" id="" class="form-control" placeholder="Username">    
+              </div>
+              <div class="form-group">
+                <input type="password" name="" id="" class="form-control" placeholder="Password">
+              </div>
+              <div class="form-group">
+                <button type="submit" class="btn btn-primary"><i class="fas fa-sign-in-alt">Login</i></button>
+              </div>
+              <div class="col-12 forgot">
+            <a href="#">crear cuenta</a>
+          </div>
+            </div>
+           
+          </form>
+         
+        </div><!--end modal content-->
+      </div>
+    </div>
+ 
+</body>
+</html>
